b19d89beb5b0bcc875c7fac869f35d8c88a40875
dd00697 <dd00697@surrey.ac.uk>
2024-05-23 23:09:26 +0100

    g

--- api/deploy.sh
+++ api/deploy.sh
@@ -4,13 +4,13 @@
 docker build -t my_model:latest .
 
 # Tag the Docker image
-docker tag my_model:latest dd00697/my_model:latest
+docker tag my_model:latest dd000697/my_model:latest
 
 # Log in to Docker Hub
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 
 # Push the Docker image to Docker Hub
-docker push dd00697/my_model:latest
+docker push dd000697/my_model:latest
 
 # Deploy the Docker container
 docker-compose down
