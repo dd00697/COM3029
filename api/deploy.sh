9e823598c5d3bdc0c5e679ec30b6a8fb285e5ce3
dd00697 <dd00697@surrey.ac.uk>
2024-05-24 03:00:02 +0100

    f

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-# Ensure the script exits if any command fails
-set -e
-
 # Build the Docker image
-echo "Building Docker image..."
 docker build -t my_model:latest .
 
 # Tag the Docker image
-echo "Tagging Docker image..."
 docker tag my_model:latest dd000697/my_model:latest
 
 # Log in to Docker Hub
-echo "Logging into Docker Hub..."
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 
 # Push the Docker image to Docker Hub
-echo "Pushing Docker image to Docker Hub..."
 docker push dd000697/my_model:latest
 
 # Deploy the Docker container
-echo "Deploying Docker container..."
 docker-compose down
 docker-compose up -d
-
-echo "Deployment completed successfully."
